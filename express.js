c4d3c7680dfd1c25d592238a9d6cd659742333b7
lzexmpoane <1939973351@qq.com>
2023-10-25 15:57:03 +0800

    aaa

--- express.js
+++ express.js
@@ -1,6 +1,6 @@
 let express = require("express")
 let app = express();
-let server = app.listen(5212, function () {
+let server = app.listen(1234, function () {
     let port = server.address().port;
     app.use(express.static('.'));
 });
